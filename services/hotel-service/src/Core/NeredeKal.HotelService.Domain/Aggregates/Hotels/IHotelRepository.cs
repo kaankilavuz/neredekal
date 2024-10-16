2046c6d9a389feb4991e86802bef1d8dc667db22
kaank <kaankilavuz1905@hotmail.com>
2024-10-16 22:59:58 +0300

    fixed

--- services/hotel-service/src/Core/NeredeKal.HotelService.Domain/Aggregates/Hotels/IHotelRepository.cs
+++ services/hotel-service/src/Core/NeredeKal.HotelService.Domain/Aggregates/Hotels/IHotelRepository.cs
@@ -4,6 +4,7 @@ namespace NeredeKal.HotelService.Domain.Aggregates.Hotels
 {
     public interface IHotelRepository : IRepository<Hotel, Guid>
     {
-
+        Task<List<Hotel>> GetPagedListAsync(string filter, int skipCount, int maxResultCount, CancellationToken cancellationToken);
+        Task<int> GetCountAsync(string filter, CancellationToken cancellationToken);
     }
 }
